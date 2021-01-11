ddbb565bcfd311a8ff65fc72f536c35d163b4a52
Carlos Plaza <carlosfplaza@hotmail.com>
2021-01-11 16:50:44 -0300

    se agrega metodo de ordenamiento

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 let randomNumber = () => {
-    let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13"]
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
     let cardNumber = Math.floor(Math.random() * numbers.length);
     return numbers[cardNumber];
 };
@@ -33,8 +33,54 @@ draw.addEventListener("click", (e) => {
         cardDiv.appendChild(cardSpan)
         document.querySelector(".cardContainer").appendChild(cardDiv)
 
-        cardsArr.push([newNumber,newSuit])
+        cardsArr.push([newNumber, newSuit])
+
     };
+
+    console.log(cardsArr);
 });
 
 
+
+const orderNumbers = () => {
+    let newArrCards = cardsArr.slice();
+    bubbleSort(newArrCards);
+    console.log(newArrCards);
+
+}
+
+const bubbleSort = (arr) => {
+    let wall = arr.length - 1;
+    while (wall > 0) {
+        let index = 0;
+        while (index < wall) {
+            if (arr[index] > arr[index + 1]) {
+                let aux = arr[index];
+                arr[index] = arr[index + 1];
+                arr[index + 1] = aux;
+                for (let j = 0; j < arr.length; j++) {
+                    // console.log(arr[j]);
+                    let cardDiv = document.createElement("div");
+                    let newNumber = arr[j][0];
+                    let newSuit = arr[j][1];
+                    cardDiv.classList.add("card");
+                    cardDiv.classList.add(newSuit);
+                    let cardSpan = document.createElement("span");
+                    cardSpan.classList.add("number");
+                    cardSpan.innerHTML = newNumber;
+                    cardDiv.appendChild(cardSpan)
+                    let cards = document.querySelector(".cardContainer2")
+                    cards.appendChild(cardDiv);
+                    if (j == arr.length - 1) {
+                        let br = document.createElement("br");
+                        cards.appendChild(br);
+                    }
+                  }
+            }
+            index++;
+        }
+        wall--;
+    }
+
+    return arr;
+};
